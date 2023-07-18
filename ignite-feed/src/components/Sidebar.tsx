5253e3658eefead9c5e7ef6877f2383c4fbb68e2
Weilemann <yuri.weilemann@impar.com.br>
2023-07-18 19:38:32 -0300

    style: Finished Sidebar style

--- ignite-feed/src/components/Sidebar.tsx
+++ ignite-feed/src/components/Sidebar.tsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react';
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -5,17 +7,20 @@ export function Sidebar() {
         <aside className={styles.sidebar}>
             <img
                 className={styles.cover}
-                src="https://avatars.githubusercontent.com/u/55397252?s=400&u=8cdfc8b57652f96e374fdbc3b9017354dea44824&v=4"
-                alt="Yuri Weilemann"
+                src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
+                alt="Cover Picture"
             />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/weilemann.png" alt="Profile Picture" />
+
                 <strong>Yuri Weilemann</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine size={20} />
                     Editar seu perfil
                 </a>
             </footer>
