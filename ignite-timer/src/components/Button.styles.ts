d600fecf2eaa920beaddd9aba2de1ef8556bfd95
Weilemann <yuri_weilemann@hotmail.com>
2023-07-25 19:23:17 -0300

    styles: Created initial theme and global style using styled components

---
+++ ignite-timer/src/components/Button.styles.ts
@@ -0,0 +1,27 @@
+import styled, { css } from 'styled-components';
+
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
+
+interface IButtonContainerProps {
+    variant: ButtonVariant;
+}
+
+const buttonVariants = {
+    primary: 'purple',
+    secondary: 'orange',
+    danger: 'red',
+    success: 'green',
+}
+
+export const ButtonContainer = styled.button<IButtonContainerProps>`
+    width: 100px;
+    height: 40px;
+    border-radius: 4px;
+    border: 0;
+    margin: 8px;
+
+    background-color: ${props => props.theme['green-500']};
+    color: ${props => props.theme.white};
+
+    /* ${props => css`background-color: ${buttonVariants[props.variant]}`} */
+`;
