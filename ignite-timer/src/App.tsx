3d811155385e6cbe0e02f5e7c8b75ab605c642bf
Weilemann <yuri_weilemann@hotmail.com>
2023-07-25 19:39:53 -0300

    refactor: Configured ESLint for coding pattern | Fixes ESLint Errors

--- ignite-timer/src/App.tsx
+++ ignite-timer/src/App.tsx
@@ -1,16 +1,16 @@
-import { ThemeProvider } from 'styled-components';
-import { Button } from './components/Button';
+import { ThemeProvider } from 'styled-components'
+import { Button } from './components/Button'
 
-import { defaultTheme } from './styles/themes/default';
-import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant='primary' />
-      <Button variant='danger' />
-      <Button variant='secondary' />
-      <Button variant='success' />
+      <Button variant="primary" />
+      <Button variant="danger" />
+      <Button variant="secondary" />
+      <Button variant="success" />
 
       <GlobalStyle />
     </ThemeProvider>
--- ignite-timer/src/components/Button.styles.ts
+++ ignite-timer/src/components/Button.styles.ts
@@ -1,27 +1,29 @@
-import styled, { css } from 'styled-components';
+import styled, { css } from 'styled-components'
 
-export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
 
 interface IButtonContainerProps {
-    variant: ButtonVariant;
+  variant: ButtonVariant
 }
 
 const buttonVariants = {
-    primary: 'purple',
-    secondary: 'orange',
-    danger: 'red',
-    success: 'green',
+  primary: 'purple',
+  secondary: 'orange',
+  danger: 'red',
+  success: 'green',
 }
 
 export const ButtonContainer = styled.button<IButtonContainerProps>`
-    width: 100px;
-    height: 40px;
-    border-radius: 4px;
-    border: 0;
-    margin: 8px;
+  width: 100px;
+  height: 40px;
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
 
-    background-color: ${props => props.theme['green-500']};
-    color: ${props => props.theme.white};
+  background-color: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme.white};
 
-    /* ${props => css`background-color: ${buttonVariants[props.variant]}`} */
-`;
+  /* ${(props) => css`
+    background-color: ${buttonVariants[props.variant]};
+  `} */
+`
